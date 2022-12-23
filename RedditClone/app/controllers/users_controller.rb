3f68e8beb793369e2eebe8d935874ff77faefa47
Stiven Kang <kdaegyung@gmail.com>
2022-12-23 14:22:56 -0500

    finished phase 1

--- RedditClone/app/controllers/users_controller.rb
+++ RedditClone/app/controllers/users_controller.rb
@@ -5,19 +5,47 @@ class UsersController < ApplicationController
 
     def new
         @user = User.new
-        redirect_to new_session_url
+        # redirect_to new_user_url
+        render :new
     end
 
     def destroy
+        @user = current_user
+        logout!
+        @user.destroy
+        render :new
+        
+        # render :new is faster than redirecting since it doesn't make another HTTP request
+        # redirect_to new_user_url
     end
 
     def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login(@user)
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
     end
 
     def index
+        @users = User.all
     end
 
     def show
         @user = User.find_by(id: params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+    def require_logged_in?
+        redirect_to new_user_url unless logged_in?
     end
 end
